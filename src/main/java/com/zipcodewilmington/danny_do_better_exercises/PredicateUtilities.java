016be101bf65e3b7610b8962e92b687233a1b438
Zanetta Norris <znorris@MacBook-Pro.localdomain>
2020-02-26 10:33:49 -0500

    Coded Predicate utilities, tests pass.

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/PredicateUtilities.java
@@ -10,7 +10,7 @@ public class PredicateUtilities {
      * @return true if `x` is greater than `y`
      */
     public Boolean isGreaterThan(int x, int y) {
-        return null;
+        return (x > y);
     }
 
     /**
@@ -19,7 +19,8 @@ public class PredicateUtilities {
      * @return true if `x` is less than `y`
      */
     public Boolean isLessThan(int x, int y) {
-        return null;
+        return (x < y);
+
     }
 
     /**
@@ -28,7 +29,7 @@ public class PredicateUtilities {
      * @return true if `x` is greater than or equal to `y`
      */
     public Boolean isGreaterThanOrEqualTo(int x, int y) {
-        return null;
+        return (x >= y);
     }
 
     /**
@@ -37,22 +38,22 @@ public class PredicateUtilities {
      * @return true if `x` is less than or equal to `y`
      */
     public Boolean isLessThanOrEqualTo(int x, int y) {
-        return null;
+        return (x <= y);
     }
-
 
     /**
      * @return true
      */
     public Boolean returnTrue() {
-        return null;
+        return true;
+
     }
 
     /**
      * @return false
      */
     public Boolean returnFalse() {
-        return null;
+        return false;
     }
 
 }
